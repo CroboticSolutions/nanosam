54caf0e7116e509666f4ab9abfd9334af88d6a94
Ana Batinovic <ana.batinovic.ab@gmail.com>
2025-09-26 14:27:33 +0200

    Change paths in the script to build the TensorRT engine

--- nanosam/build_engine.sh
+++ nanosam/build_engine.sh
@@ -2,14 +2,16 @@
 
 # Build the TensorRT engine for the SAM mask decoder
 trtexec \
-    --onnx=data/mobile_sam_mask_decoder.onnx \
-    --saveEngine=data/mobile_sam_mask_decoder.engine \
-    --minShapes=point_coords:1x1x2,point_labels:1x1 \
-    --optShapes=point_coords:1x1x2,point_labels:1x1 \
-    --maxShapes=point_coords:1x10x2,point_labels:1x10
+  --onnx=/workspace/nanosam/data/mobile_sam_mask_decoder.onnx \
+  --saveEngine=/workspace/nanosam/data/mobile_sam_mask_decoder.engine \
+  --minShapes=point_coords:1x1x2,point_labels:1x1 \
+  --optShapes=point_coords:1x1x2,point_labels:1x1 \
+  --maxShapes=point_coords:1x10x2,point_labels:1x10
 
 # Build the TensorRT engine for the NanoSAM image encoder
 trtexec \
-    --onnx=data/resnet18_image_encoder.onnx \
-    --saveEngine=data/resnet18_image_encoder.engine \
+    --onnx=/workspace/nanosam/data/resnet18_image_encoder.onnx \
+    --saveEngine=/workspace/nanosam/data/resnet18_image_encoder.engine \
     --fp16
+
+exec /bin/bash
